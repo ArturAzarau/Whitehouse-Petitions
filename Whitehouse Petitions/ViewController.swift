22d4a10d9586f955467bef27f3644de312aa025e
ArturAzarau <artur.azarau@gmail.com>
2018-08-02 11:56:56 +0300

    Initial Commit

---
+++ Whitehouse Petitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whitehouse Petitions
+//
+//  Created by Артур Азаров on 02.08.2018.
+//  Copyright © 2018 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
